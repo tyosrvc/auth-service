d6f68544191f0920e4ba571025edbf886e9c3122
tyosrvc <prasetyoadi@mykolab.com>
2026-03-04 16:25:17 +0700

    Create man.py

---
+++ man.py
@@ -0,0 +1,155 @@
+import time
+from machine import Pin
+
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+for i in range(10):
+    time.sleep(interval)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)    led.value(off)
+    time.sleep(interval)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)    led.value(on)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+for i in range(10):
+    time.sleep(interval)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)    led.value(off)
+    time.sleep(interval)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)    led.value(on)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+
+led.value(on)
+led = Pin(2, Pin.OUT)
+
+on = 0
+off = 1
+interval = .5
+
+led.value(on)
+
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)
+for i in range(10):
+    time.sleep(interval)
+    led.value(off)
+    time.sleep(interval)
+    led.value(on)
+    
+time.sleep(interval)
+led.value(off)    
+time.sleep(interval)
+led.value(off)
